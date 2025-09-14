c3e29c24d0f642cf65b2ef932bb6784bf5883b5c
dhyan-murphy <dhyan@murphy.inc>
2025-09-14 13:28:17 +0530

    cors added

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  )
+);
 app.use(express.json());
 
 // Connect DB
