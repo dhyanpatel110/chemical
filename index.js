abcbad49ce42e6e31687af6168438d5416710a5d
dhyan-murphy <dhyan@murphy.inc>
2025-09-14 13:31:28 +0530

    remove cors origin

--- index.js
+++ index.js
@@ -10,15 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors(
-    cors({
-      origin: "*",
-      methods: ["GET", "POST"],
-      credentials: true,
-    })
-  )
-);
+app.use(cors());
 app.use(express.json());
 
 // Connect DB
